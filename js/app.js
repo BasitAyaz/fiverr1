66551c67e6de0e6512a87fb08e117f20ce65e936
BasitAhmed <basitahmed1997@gmail.com>
2020-11-16 14:59:04 -0800

    done

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 var percentage = 0;
 var totalNumber = 11;
-
-function next() {
+var nextSlideNo = 2;
+var previousSlideNo = 1;
+function next(e) {
   percentage++;
+  nextSlideNo++;
+  previousSlideNo++;
   var annual = (percentage / totalNumber) * 100;
   var final = Math.round(annual);
   console.log(final);
@@ -11,7 +14,21 @@ function next() {
   var firstName = document.getElementById("first_Name").value;
   document.getElementById("firstName1").innerHTML = firstName;
   document.getElementById("firstName2").innerHTML = firstName;
-  var elmnt = document.getElementById("slide2");
+  var num = e;
+  console.log(num);
+  var ref = `slide${num}`;
+  console.log(ref);
+  var elmnt = document.getElementById(ref);
+  elmnt.scrollIntoView();
+  window.location.href = `#slide${num}`;
+
+}
+function nextSlide() {
+  var elmnt = document.getElementById(`slide${nextSlideNo}`);
+  elmnt.scrollIntoView();
+}
+function previousSlide() {
+  var elmnt = document.getElementById(`slide${previousSlideNo}`);
   elmnt.scrollIntoView();
 }
 function select(e) {
