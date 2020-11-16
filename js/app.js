49eebaf5d0431939cbe588be99d7cabb2b92efb0
BasitAhmed <basitahmed1997@gmail.com>
2020-11-16 14:31:00 -0800

    Done

--- js/app.js
+++ js/app.js
@@ -1,20 +1,22 @@
 var percentage = 0;
-var totalNumber = 12;
+var totalNumber = 11;
 
-function next(){
-    percentage++
-    var annual = (percentage/totalNumber) *100;
-    var final = Math.round(annual)
-    console.log(final)
-    document.getElementById('showPercent').style.width = `${final}%`
-    document.getElementById('percentageNumber').innerHTML = final
-    var firstName = document.getElementById('first_Name').value
-    document.getElementById('firstName1').innerHTML = firstName
-    document.getElementById('firstName2').innerHTML = firstName
+function next() {
+  percentage++;
+  var annual = (percentage / totalNumber) * 100;
+  var final = Math.round(annual);
+  console.log(final);
+  document.getElementById("showPercent").style.width = `${final}%`;
+  document.getElementById("percentageNumber").innerHTML = final;
+  var firstName = document.getElementById("first_Name").value;
+  document.getElementById("firstName1").innerHTML = firstName;
+  document.getElementById("firstName2").innerHTML = firstName;
+  var elmnt = document.getElementById("slide2");
+  elmnt.scrollIntoView();
 }
-function select(e){
-    e.className = 'selected'
+function select(e) {
+  e.className = "selected";
 }
-function showCVinput(){
-    document.getElementById('input_CV').style.display = 'block'
+function showCVinput() {
+  document.getElementById("input_CV").style.display = "block";
 }
